3c0e853db2ffbfc413d37e4e38b4dcaf336dc67d
Sojib Ahmed <sojiba1500@gmail.com>
2026-01-16 12:16:29 +0800

    Fix firebaseConfig export and update storageBucket

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
-// firebase-config.js
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyB6b93A_HeU4FADs3o2Ysw6-dlRRS2TbZk",
   authDomain: "telegram-miniapp-e8cc0.firebaseapp.com",
   databaseURL: "https://telegram-miniapp-e8cc0-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "telegram-miniapp-e8cc0",
-  storageBucket: "telegram-miniapp-e8cc0.appspot.com",
+  storageBucket: "telegram-miniapp-e8cc0.firebasestorage.app",
   messagingSenderId: "827930913054",
   appId: "1:827930913054:web:502b56e0c198d8e9ff410f"
 };
+
+firebase.initializeApp(firebaseConfig);
