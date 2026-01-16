e021a961850dae1050ae5a4e63cedeb424d897a2
Sojib Ahmed <sojiba1500@gmail.com>
2026-01-16 23:23:12 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,24 +1,31 @@
-import { auth, provider } from "./firebase-config.js";
+import { auth, db } from "./firebase-config.js";
 import {
-  createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  signInWithPopup
+  createUserWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { ref, set } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-window.signup = () => {
-  createUserWithEmailAndPassword(auth, email.value, password.value)
+window.login = function () {
+  const email = email.value;
+  const password = password.value;
+
+  signInWithEmailAndPassword(auth, email, password)
     .then(() => location.href = "dashboard.html")
     .catch(e => alert(e.message));
 };
 
-window.login = () => {
-  signInWithEmailAndPassword(auth, email.value, password.value)
-    .then(() => location.href = "dashboard.html")
-    .catch(e => alert(e.message));
-};
+window.register = function () {
+  const email = email.value;
+  const password = password.value;
 
-window.googleLogin = () => {
-  signInWithPopup(auth, provider)
-    .then(() => location.href = "dashboard.html")
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(res => {
+      set(ref(db, "users/" + res.user.uid), {
+        email,
+        coins: 0,
+        level: 1
+      });
+      location.href = "dashboard.html";
+    })
     .catch(e => alert(e.message));
 };
